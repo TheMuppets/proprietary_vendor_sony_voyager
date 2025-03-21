661fad248caa14fc1080170ab829ae80df948081
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-21 07:52:53 +0700

    voyager: Drop false libmm-qcamera dependency from libmmcamera_tuning
    Change-Id: I127daaf3d0e5cd925a01bdbdbec35894d8a87be7

--- voyager-vendor.mk
+++ voyager-vendor.mk
@@ -202,7 +202,6 @@ PRODUCT_PACKAGES += \
     libjpegdhw \
     libjpegdmahw \
     libjpegehw \
-    libmm-qcamera \
     libmmcamera2_c2d_module \
     libmmcamera2_cpp_module \
     libmmcamera2_frame_algorithm \
