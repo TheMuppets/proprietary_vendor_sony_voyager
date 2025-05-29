160edce455d20404306ee9805f383c86dcf97394
LuK1337 <priv.luk@gmail.com>
2025-05-29 10:47:27 +0200

    voyager: Get rid of unused 32-bit libdevice_security_vendor.so
    Change-Id: Ie87b8c58f6038340e487527b73e1f8011cdf5f21

--- voyager-vendor.mk
+++ voyager-vendor.mk
@@ -131,7 +131,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     libcdsprpc \
-    libdevice_security_vendor \
     libsuntory \
     vendor.somc.hardware.security.secd@1.0 \
     camera.sdm660 \
@@ -302,6 +301,7 @@ PRODUCT_PACKAGES += \
     libqomx_jpegenc_pipe \
     libremosaic_daemon \
     android.hardware.keymaster@3.0-impl-qti \
+    libdevice_security_vendor \
     cnss-daemon \
     android.hardware.keymaster@3.0-service-qti \
     vendor.semc.hardware.secd@1.0-service \
